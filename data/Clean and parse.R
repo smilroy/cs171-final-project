9344b1541a01e1e0df437d7afb40bc12841a2d2b
smilroy <shannon.milroy@mail.harvard.edu>
2015-04-10 15:02:29 -0400

    Add UN population data

--- data/Clean and parse.R
+++ data/Clean and parse.R
@@ -1,11 +1,16 @@
 ##Set working directory
-setwd("/Users/shannonmilroy/Documents/Harvard School Files/Spring 2015/Visualization/cs171-final-project/data/")
+setwd("/Users/shannonmilroy/Documents/Harvard School Files/Spring 2015/Visualization/cs171-final-project/data/Original data")
 
 ##Load data
 data <- read.csv("IHME_GBD_2013_MDG6_1990_2013_HIV_TB_INCIDENCE_PREVALENCE_DEATHS_Y2014M11D21.CSV")
 
 ##Subset to HIV/AIDS
-data <- subset(data, cause_name=="HIV/AIDS", select = c(location_name, year, age_group_name, sex_name, cause_name, metric, mean, lower, upper))
+data <- subset(data, cause_name=="HIV/AIDS", select = c(location_name, year, age_group_id,age_group_name, sex_name, cause_name, metric, mean, lower, upper))
 
 ##Write to csv
-write.csv(data, "HIVAIDS_data_clean")
+write.csv(data, "HIVAIDS_data_clean.csv")
+
+##Write to xls
+global <- subset(data,data$sex_name=="Both sexes"&data$location_name=="Sub-Saharan Africa"&data$metric=="Deaths"&(data$age_group_id<22&data$age_group_id>8))
+library(xlsx)
+write.xlsx(global, "HIVAIDS_data_clean2.xls")
--- process-book.md
+++ process-book.md
@@ -9,6 +9,12 @@ Final Project
 
 ### Data
 
+Sources:
+
+HIV Data: (http://ghdx.healthdata.org/record/global-burden-disease-study-2013-gbd-2013-hiv-tuberculosis-and-malaria-incidence-prevalence)
+
+Population Data: http://esa.un.org/wpp/Excel-Data/population.htm
+
 ### Exploratory Data Analysis
 
 ### Design Evolution
